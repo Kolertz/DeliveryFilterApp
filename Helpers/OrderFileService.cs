606e0697139c4a11392e90a93b13aa74a9147731
Kolerts <strelok1strelok@gmail.com>
2024-10-24 14:37:30 +0800

    Added line skipping in case of incorrect data in file

--- Helpers/OrderFileService.cs
+++ Helpers/OrderFileService.cs
@@ -16,14 +16,43 @@ namespace DeliveryFilterApp.Helpers
                 foreach (var line in File.ReadLines(filePath))
                 {
                     var parts = line.Split(',');
-                    var order = new OrderModel
+
+                    // Проверка, что строка содержит все необходимые части
+                    if (parts.Length < 4)
                     {
-                        OrderId = parts[0],
-                        Weight = double.Parse(parts[1]),
-                        CityDistrict = parts[2],
-                        DeliveryDateTime = DateTime.ParseExact(parts[3], _configuration["DeliveryServiceSettings:DateFormat"]!, null)
-                    };
-                    orders.Add(order);
+                        _logger.LogWarning("Строка пропущена из-за недостаточного количества полей: {Line}", line);
+                        continue;
+                    }
+
+                    try
+                    {
+                        var order = new OrderModel
+                        {
+                            OrderId = parts[0],
+
+                            Weight = double.TryParse(parts[1], out var weight)
+                                ? weight
+                                : throw new FormatException("Некорректный формат веса"),
+
+                            CityDistrict = parts[2],
+
+                            DeliveryDateTime = DateTime.TryParseExact(
+                                parts[3],
+                                _configuration["DeliveryServiceSettings:DateFormat"]!,
+                                null,
+                                System.Globalization.DateTimeStyles.None,
+                                out var deliveryDateTime)
+                                ? deliveryDateTime
+                                : throw new FormatException("Некорректный формат даты")
+                        };
+
+                        orders.Add(order);
+                    }
+                    catch (Exception innerEx)
+                    {
+                        // Логируем ошибку и продолжаем выполнение с пропуском этой строки
+                        _logger.LogWarning(innerEx, "Ошибка в данных строки: {Line}. Строка пропущена.", line);
+                    }
                 }
             }
             catch (Exception ex)
